2eb615bfbf50430150c64bf5385157cab0294c5b
flypigamer <degssfgsd@gmail.com>
2026-09-03 19:41:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const AdmZip = require("adm-zip");
 const fs = require("fs");
 const { Pool } = require("pg");
 
+const crypto = require("crypto");
+
+function encryptToken(token) {
+  const key = crypto
+    .createHash("sha256")
+    .update(process.env.BOT_ENCRYPTION_KEY)
+    .digest();
+
+  const iv = crypto.randomBytes(16);
+
+  const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+
+  let encrypted = cipher.update(token, "utf8", "hex");
+  encrypted += cipher.final("hex");
+
+  return iv.toString("hex") + ":" + encrypted;
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
